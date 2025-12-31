a61ff70b78c14e7aad9d753515b4633c861751c1
Yicong Li <yicongli2005@outlook.com>
2025-12-31 16:04:40 -0500

    Set up database to store user information and tasks

--- database/fasttaskdb.sql
+++ database/fasttaskdb.sql
@@ -1,5 +1,22 @@
+-- Active: 1767155769831@@localhost@3306@fasttaskdb
 CREATE TABLE IF NOT EXISTS users (
-    id PRIMARY KEY email VARCHAR(255) NOT NULL,
-    user_password VARCHAR(20) NOT NULL,
-    username VARCHAR(20) NOT NULL
+    email VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    date_of_birth DATE,
+    profile_picture_url VARCHAR(255),
+    PRIMARY KEY(email)
+);
+
+CREATE TABLE IF NOT EXISTS tasks (
+    taskID VARCHAR(36) NOT NULL,
+    taskName VARCHAR(100) NOT NULL,
+    notes TEXT,
+    deadline DATETIME,
+    remindAt DATETIME,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY(taskID),
+    FOREIGN KEY(email) REFERENCES users(email)
 );
