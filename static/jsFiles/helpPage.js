b8ea6df6719018ddaedac2cff9b826173bbd2984
Yicong Li <yicongli2005@outlook.com>
2026-05-14 21:52:18 -0400

    Finished help page and cleaned up password recovery page

---
+++ static/jsFiles/helpPage.js
@@ -0,0 +1,13 @@
+let collapsibles = document.getElementsByClassName("collapsible");
+
+for (let i=0; i < collapsibles.length; i++) {
+    collapsibles[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
